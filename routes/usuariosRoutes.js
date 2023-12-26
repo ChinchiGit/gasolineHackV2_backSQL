c05decf7fb67003de6cb593da6dffcd9becc2a36
unknown <you@example.com>
2023-12-26 11:09:29 +0100

    20231226 deploy

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -3,14 +3,14 @@ const usuariosRouter = express.Router();
 const usuariosControllers = require('../controllers/usuariosControllers');
 
 
-// usuariosRouter.get('/all', usuariosControllers.obtenerusuarios);
+
 usuariosRouter.get('/get-one/:email', usuariosControllers.obtenerUsuarioPorEmail);
 usuariosRouter.post('/create', usuariosControllers.crearUsuario);
-// usuariosRouter.put('/update-one', usuariosControllers.actualizarAutor);
+
 usuariosRouter.delete('/delete-one',usuariosControllers.borrarUsuario );
 usuariosRouter.delete('/delete-all', usuariosControllers.borrarTodosLosUsuarios);
 usuariosRouter.get('/all-user-gasstation', usuariosControllers.obtenerUnUsuarioYTodasSusGasolineras);
-// usuariosRouter.get('/all-authors-all-entries', usuariosControllers.obtenerTodosLosusuariosConSusEntradas);
+
 usuariosRouter.post('/populate-usuarios',usuariosControllers.poblarTablaUsuarios );
 
 
